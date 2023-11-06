60609c02fd4dd8483ca2385d02b285dfa41340c0
sharabassy <abdelrahman.elsharabassy@gmail.com>
2023-11-06 23:33:29 +0200

    printing many numbers

---
+++ printingNumbers.h
@@ -0,0 +1,14 @@
+//
+// Created by Sharabassy on 11/6/2023.
+//
+
+#ifndef OOP_ASSIGNMENTS_PRINTINGNUMBERS_H
+#define OOP_ASSIGNMENTS_PRINTINGNUMBERS_H
+
+
+class printingNumbers {
+
+};
+
+
+#endif //OOP_ASSIGNMENTS_PRINTINGNUMBERS_H
